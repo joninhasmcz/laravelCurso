52ffbec579d30b30e3ac4c4c9b57d9484e7473c9
Jonas Santos <jonas.alves@seplag.al.gov.br>
2021-09-17 12:31:51 -0300

    add css, add model event

--- cursoLaravel/resources/views/welcome.blade.php
+++ cursoLaravel/resources/views/welcome.blade.php
@@ -4,9 +4,29 @@
 
 @section('content')
 
-    <h1> Página Principal </h1>
-    <p>{{$nome}}</p>
-    <a href="/contato">Contatos</a>
-    </br>
-    <a href="/produto">Produtos</a>
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type = "text" id = "search" name="search" class="form-control" placeholser="Procurar...">
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p>Veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach($events as $event)
+                <div class="card col-md3">
+                    <img src="img/baile.jpg" alt="{{ $event -> title}}">
+                    <div class="card-body">
+                        <p class="card-date">10/05/1997</p>
+                        <h5 class="card-title">{{ $event -> title}}</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="bt btn-primary"> Saber Mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+ 
 @endsection
--- cursoLaravel/routes/web.php
+++ cursoLaravel/routes/web.php
@@ -16,18 +16,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']); 
+
 Route::get('/event/create', [EventController::class, 'create']); 
 
 Route::get('/contato', function () {
     return view('contact');
 });
-Route::get('/produto', function () {
-
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-Route::get('/produto/{id}', function ($id) {
-    return view('product', ['id' => $id]);
-});
-
